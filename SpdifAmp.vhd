19ab03d4aa7ea28a5af059493b886ee04a7a4d82
Enrico Gueli <enrico.gueli@gmail.com>
2020-05-21 17:15:46 +0200

    add BMC decoding of payload data

--- SpdifAmp.vhd
+++ SpdifAmp.vhd
@@ -7,7 +7,7 @@ entity SpdifAmp is
 	port(
 		i_clock: in std_logic;
 		i_data: in std_logic;
-		o_payload_begin, o_payload_clock, o_small, o_medium, o_large: out std_logic;
+		o_payload_begin, o_payload_clock, o_payload_data: out std_logic;
 		o_px, o_py, o_pz: out std_logic
 	);
 end SpdifAmp;
@@ -15,13 +15,38 @@ end SpdifAmp;
 architecture rtl of SpdifAmp is
 	component Aes3PreambleDecoder is
 		port(
-			i_clock: in std_logic;
-			i_data: in std_logic;
-			o_payload_begin, o_payload_clock, o_small, o_medium, o_large: out std_logic;
-			o_px, o_py, o_pz: out std_logic
+		i_clock: in std_logic;
+		i_data: in std_logic;
+		o_payload_begin, o_payload_clock, o_small, o_medium, o_large: out std_logic;
+		o_px, o_py, o_pz: out std_logic
 		);
 	end component Aes3PreambleDecoder;
+	component BiphaseMarkDecoder is
+		port(
+			i_clock: in std_logic;
+			i_strobe: in std_logic;
+			i_short: in std_logic;
+			i_long: in std_logic;
+			o_strobe: out std_logic;
+			o_data: out std_logic
+		);
+	end component BiphaseMarkDecoder;
+	
+	signal r_small, r_medium, r_large: std_logic;
+	signal r_payload_clock: std_logic;
 begin
-	decoder: Aes3PreambleDecoder port map (i_clock, i_data, o_payload_begin, o_payload_clock, o_small, o_medium, o_large, o_px, o_py, o_pz);
+	preambleDecoder: Aes3PreambleDecoder port map (
+		i_clock, 
+		i_data, 
+		o_payload_begin, r_payload_clock, r_small, r_medium, r_large, 
+		o_px, o_py, o_pz);
+		
+	bmcDecoder: BiphaseMarkDecoder port map (
+		i_clock, 
+		r_payload_clock, 
+		r_small, 
+		r_medium, 
+		o_payload_clock, 
+		o_payload_data);
 end rtl;
 
