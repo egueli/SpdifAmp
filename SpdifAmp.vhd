44d162ef99e89fc725545498321bf1625bc7da1f
Enrico Gueli <enrico.gueli@gmail.com>
2020-05-22 20:31:47 +0200

    add shift register to have all subframe bits; fix warnings

--- SpdifAmp.vhd
+++ SpdifAmp.vhd
@@ -8,7 +8,9 @@ entity SpdifAmp is
 		i_clock: in std_logic;
 		i_data: in std_logic;
 		o_payload_begin, o_payload_clock, o_payload_data: out std_logic;
-		o_px, o_py, o_pz: out std_logic
+		o_px, o_py, o_pz: out std_logic;
+		o_payload: out std_logic_vector(27 downto 0);
+		o_payload_strobe: out std_logic
 	);
 end SpdifAmp;
  
@@ -31,22 +33,52 @@ architecture rtl of SpdifAmp is
 			o_data: out std_logic
 		);
 	end component BiphaseMarkDecoder;
+	component ShiftRegister is
+		generic(n_bits : integer := 28);
+		port(
+			i_clock: in std_logic;
+			i_data: in std_logic;
+			i_strobe: in std_logic;
+			i_reset: in std_logic;
+			o_output: out std_logic_vector((n_bits - 1) downto 0);
+			o_strobe: out std_logic
+		);
+	end component ShiftRegister;
 	
 	signal r_small, r_medium, r_large: std_logic;
+	signal r_payload_clock_bmc: std_logic;
+	
+	signal r_payload_begin: std_logic;
 	signal r_payload_clock: std_logic;
+	signal r_payload_data: std_logic;
+
 begin
 	preambleDecoder: Aes3PreambleDecoder port map (
 		i_clock, 
 		i_data, 
-		o_payload_begin, r_payload_clock, r_small, r_medium, r_large, 
+		r_payload_begin, r_payload_clock_bmc, r_small, r_medium, r_large, 
 		o_px, o_py, o_pz);
 		
 	bmcDecoder: BiphaseMarkDecoder port map (
 		i_clock, 
-		r_payload_clock, 
+		r_payload_clock_bmc, 
 		r_small, 
 		r_medium, 
-		o_payload_clock, 
-		o_payload_data);
+		r_payload_clock, 
+		r_payload_data);
+		
+	o_payload_begin <= r_payload_begin;
+	o_payload_clock <= r_payload_clock;
+	o_payload_data <= r_payload_data;
+		
+	payloadShiftRegister: ShiftRegister port map (
+		i_clock => i_clock,
+		i_data => r_payload_data,
+		i_strobe => r_payload_clock,
+		i_reset => r_payload_begin,
+		o_output => o_payload,
+		o_strobe => o_payload_strobe
+		);
+	
 end rtl;
 
