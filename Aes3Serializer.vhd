469b31c99d034336d4857b2133c3b04966389a31
Enrico Gueli <enrico.gueli@gmail.com>
2020-06-18 20:53:45 +0200

    fix toggling & latching; disable parity

--- Aes3Serializer.vhd
+++ Aes3Serializer.vhd
@@ -21,9 +21,9 @@ architecture rtl of Aes3Serializer is
 		y : std_logic;
 		z : std_logic
 	) return std_logic is
-		constant PREAMBLE_X : std_logic_vector(0 to 7) := "11100010";
-		constant PREAMBLE_Y : std_logic_vector(0 to 7) := "11100100";
-		constant PREAMBLE_Z : std_logic_vector(0 to 7) := "11101000";
+		constant PREAMBLE_X : std_logic_vector(0 to 7) := "10010011";
+		constant PREAMBLE_Y : std_logic_vector(0 to 7) := "10010110";
+		constant PREAMBLE_Z : std_logic_vector(0 to 7) := "10011100";
 	begin
 		if x = '1' then
 			return PREAMBLE_X(pulse_count);
@@ -35,20 +35,36 @@ architecture rtl of Aes3Serializer is
 	end function;
 	
 	signal r_payload : std_logic_vector(31 downto 4) := (others => '0');
+	signal r_px, r_py, r_pz: std_logic;
 	signal r_pulse_count: integer range 0 to 63 := 0;
-	
+	signal r_output_toggle: std_logic := '0';
+	signal r_output: std_logic := '0';
 begin
 	serializer : process(i_pulse_clock, i_strobe) is
 	begin
 		if i_strobe = '1' then
 			r_pulse_count <= 0;
 			r_payload <= i_payload;
+			r_px <= i_px;
+			r_py <= i_py;
+			r_pz <= i_pz;
 		elsif rising_edge(i_pulse_clock) then
 			r_pulse_count <= (r_pulse_count + 1) mod 64;
 			case r_pulse_count is
-				when 0 to 7 => o_output <= make_preamble(r_pulse_count, i_px, i_py, i_pz);
-				when others => o_output <= r_payload(r_pulse_count / 2);
+				when 0 to 7 => r_output_toggle <= make_preamble(r_pulse_count, r_px, r_py, r_pz);
+				when others => 
+					if (r_pulse_count mod 2) = 0 then
+						r_output_toggle <= '1';
+					else
+						r_output_toggle <= r_payload(r_pulse_count / 2);
+					end if;
 			end case;
+			
+			if r_output_toggle = '1' then
+				r_output <= not r_output;
+			end if;
 		end if;
 	end process;
+	
+	o_output <= r_output;
 end rtl;
