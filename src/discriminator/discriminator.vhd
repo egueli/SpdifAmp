d6961ede59e833ef4c75d53d87a331706978cd85
Enrico Gueli <enrico.gueli@gmail.com>
2020-07-17 19:22:25 +0200

    discriminator: add testbench; fix

--- src/discriminator/discriminator.vhd
+++ src/discriminator/discriminator.vhd
@@ -6,7 +6,7 @@ entity discriminator is
   port (
     clk : in std_logic;
     rst : in std_logic;
-    input : in std_logic;
+    pulses : in std_logic;
     large : out std_logic;
     medium : out std_logic;
     small : out std_logic;
@@ -15,8 +15,7 @@ entity discriminator is
 end discriminator; 
 
 architecture rtl of discriminator is
-  signal input_p1 : std_logic;
-  signal transition : std_logic;
+  signal pulses_p1 : std_logic;
 
   subtype pulse_count_type is integer range 0 to 31;
   signal pulse_count : pulse_count_type;
@@ -24,18 +23,17 @@ begin
   PROC_PULSE_DISCRIMINATOR : process(clk)
   begin
     if rising_edge(clk) then
-      transition <= '0';
       large <= '0';            
       small <= '0';            
       medium <= '0';  
       valid <= '0';
-      input_p1 <= input;
 
       if rst = '1' then
+        pulses_p1 <= '0';
         pulse_count <= 0;
-        input_p1 <= input;
       else
-        if input_p1 = input then
+        pulses_p1 <= pulses;
+        if not (pulses_p1 = '0' and pulses = '1') then
           if pulse_count < pulse_count_type'high then
             pulse_count <= pulse_count + 1;
           else
@@ -45,10 +43,13 @@ begin
         else
           pulse_count <= 0;
           if pulse_count > 21 then
+            valid <= '1';
             large <= '1';
           elsif pulse_count > 13 then
+            valid <= '1';
             medium <= '1';
           elsif pulse_count > 6 then
+            valid <= '1';
             small <= '1';
           end if;
         end if;
---
+++ src/discriminator/discriminator_tb.vhd
@@ -0,0 +1,90 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use std.env.finish;
+
+library spdif_amp;
+library spdif_amp_sim;
+use spdif_amp_sim.sim_subprograms.all;
+
+entity discriminator_tb is
+end discriminator_tb; 
+
+architecture sim of discriminator_tb is
+  signal clk : std_logic := '1';
+  signal rst : std_logic := '1';
+  signal pulses : std_logic := '0';
+  signal large : std_logic;
+  signal medium : std_logic;
+  signal small : std_logic;
+  signal valid : std_logic;
+begin
+  DUT : entity spdif_amp.discriminator(rtl)
+  port map (
+    clk => clk,
+    rst => rst,
+    pulses => pulses,
+    large => large,
+    medium => medium,
+    small => small,
+    valid => valid
+  );
+
+  gen_clock(clk);
+
+  PROC_SEQUENCER : process
+    procedure send_pulse is
+    begin
+      pulses <= '1';
+      wait until rising_edge(clk);
+      pulses <= '0';
+      wait until rising_edge(clk); 
+    end procedure;
+  begin
+    do_reset(clk, rst);
+
+    send_pulse;
+    SMALL_PULSE : for i in 0 to 8 loop
+      wait until rising_edge(clk);
+    end loop; -- SMALL_PULSE
+    send_pulse;
+
+    assert valid = '1'
+      report "not valid after small pulse"
+      severity failure;
+    
+    assert small = '1' and medium = '0' and large = '0'
+      report "small pulse not correctly identified"
+      severity failure;
+
+    MEDIUM_PULSE : for i in 0 to 16 loop
+      wait until rising_edge(clk);
+    end loop; -- MEDIUM_PULSE
+    send_pulse;
+
+    assert valid = '1'
+      report "not valid after medium pulse"
+      severity failure;
+    
+    assert small = '0' and medium = '1' and large = '0'
+      report "medium pulse not correctly identified"
+      severity failure;
+    
+    LARGE_PULSE : for i in 0 to 25 loop
+      wait until rising_edge(clk);
+    end loop; -- LARGE_PULSE
+    send_pulse;
+
+    assert valid = '1'
+      report "not valid after large pulse"
+      severity failure;
+    
+    assert small = '0' and medium = '0' and large = '1'
+      report "large pulse not correctly identified"
+      severity failure;
+
+    print_test_ok;
+    finish;
+  end process; -- PROC_SEQUENCER
+end architecture;
