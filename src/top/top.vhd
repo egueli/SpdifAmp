1364451fdfe9268a0c0dfffb3c6ace5146397dc3
Enrico Gueli <enrico.gueli@gmail.com>
2020-08-01 08:38:52 +0200

    add basic top module

---
+++ src/top/top.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity top is
+  port (
+    clk : in std_logic;
+    rst : in std_logic;
+    input : in std_logic;
+    output : out std_logic
+  );
+end top; 
+
+architecture str of top is
+
+begin
+
+end architecture;
