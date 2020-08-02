181eb344b342136602d20cf4d37546402ffe174c
Enrico Gueli <enrico.gueli@gmail.com>
2020-08-02 11:49:11 +0200

    add reset to top module

--- src/top/top.vhd
+++ src/top/top.vhd
@@ -2,17 +2,24 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library spdif_amp;
+
 entity top is
   port (
     clk : in std_logic;
-    rst : in std_logic;
+    rst_button : in std_logic;
     input : in std_logic;
     output : out std_logic
   );
 end top; 
 
 architecture str of top is
-
+  signal rst : std_logic;
 begin
-
+  RESET : entity spdif_amp.reset(rtl)
+  port map (
+    clk => clk,
+    rst_in => rst_button,
+    rst_out => rst
+  );
 end architecture;
