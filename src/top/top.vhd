6bba3ee5eb6f77a210eab172a189b16c61872812
Enrico Gueli <enrico.gueli@gmail.com>
2020-08-03 09:11:12 +0200

    add encoder & decoder to top module

--- src/top/top.vhd
+++ src/top/top.vhd
@@ -15,11 +15,41 @@ end top;
 
 architecture str of top is
   signal rst : std_logic;
+
+  signal subframe_type_x : std_logic;
+  signal subframe_type_y : std_logic;
+  signal subframe_type_z : std_logic;
+  signal subframe_payload : std_logic_vector(27 downto 0);
+  signal subframe_valid : std_logic;
 begin
   RESET : entity spdif_amp.reset(rtl)
   port map (
     clk => clk,
     rst_in => rst_button,
     rst_out => rst
+  );
+
+  DECODER : entity spdif_amp.aes3_decoder(str)
+  port map (
+    clk => clk,
+    rst => rst,
+    input => input,
+    subframe_type_x => subframe_type_x,
+    subframe_type_y => subframe_type_y,
+    subframe_type_z => subframe_type_z,
+    subframe_payload => subframe_payload,
+    subframe_valid => subframe_valid
+  );
+
+  ENCODER : entity spdif_amp.aes3_encoder(str)
+  port map (
+    clk => clk,
+    rst => rst,
+    payload => subframe_payload,
+    type_x => subframe_type_x,
+    type_y => subframe_type_y,
+    type_z => subframe_type_z,
+    subframe_valid => subframe_valid,
+    output => output
   );
 end architecture;
