af2567c0cfe85291f54afb26fb925fbf2ec34840
Enrico Gueli <enrico.gueli@gmail.com>
2020-10-04 18:46:19 +0200

    introduce SPI types

--- src/packages/types_pck.vhd
+++ src/packages/types_pck.vhd
@@ -6,4 +6,14 @@ package types is
   subtype subframe_payload_t is std_logic_vector(27 downto 0);
 
   type subframe_type_t is (TYPE_X, TYPE_Y, TYPE_Z);
+
+  type spi_slave_in_t is record
+    sclk : std_logic;
+    ss : std_logic;
+    mosi : std_logic;
+  end record spi_slave_in_t;
+
+  type spi_slave_out_t is record
+    miso : std_logic;
+  end record spi_slave_out_t;
 end package;
