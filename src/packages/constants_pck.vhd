f41730763665893ad7eeab090baf310d579917cf
Enrico Gueli <enrico.gueli@gmail.com>
2020-11-08 16:56:07 +0100

    add max_window entity and TB

--- src/packages/constants_pck.vhd
+++ src/packages/constants_pck.vhd
@@ -1,5 +1,7 @@
 package constants is
-  -- FPGA board frequency
+  -- FPGA board frequency in Hz
   constant clock_frequency : real := 50.0e6;
 
+  -- VU-meter update frequency, as log2 scale from FPGA clock
+  constant vu_meter_update_log2_count : integer := 18;
 end package;
