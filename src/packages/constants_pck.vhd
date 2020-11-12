05a034a5142a177d06e7ba8b3f6a698f98801838
Enrico Gueli <enrico.gueli@gmail.com>
2020-11-12 19:04:32 +0100

    fix EOF and comment

--- src/packages/constants_pck.vhd
+++ src/packages/constants_pck.vhd
@@ -3,5 +3,5 @@ package constants is
   constant clock_frequency : real := 50.0e6;
 
   -- VU-meter update frequency, as log2 scale from FPGA clock
-  constant vu_meter_update_log2_count : integer := 18;
+  constant vu_meter_update_log2_count : integer := 18;  -- 50MHz / 2**18 = ~190Hz
 end package;
